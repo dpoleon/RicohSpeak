1c651b3869513b84d1d78f6860b9ec0a6e3dd368
dpoleon <dpoleon@wm-mp.co.uk>
2015-10-17 15:55:31 +0100

    Project files added
    Project files added

---
+++ RicohSpeak.device/app.js
@@ -0,0 +1,17 @@
+﻿var cylon = require('cylon');
+var socket = require('socket.io-client')
+    .connect('http://RicohSpeak.azurewebsites.net');
+
+//work on a real device
+cylon.robot({
+    name: 'edison',
+    connections: { edison: { adaptor: 'intel-iot' } },
+    devices: { printer: { driver: 'direct-pin', pin: 2 } },
+    work: function (edison) {
+        socket.emit('setTarget');
+        socket.on('command', function (cmd) {
+            edison.printer.digitalWrite(1);
+            setTimeout(function () { edison.printer.digitalWrite(0); }, 2000);
+        })
+    }
+}).start();
---
+++ RicohSpeak.device/gulpconfig.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+    targetDevices: [
+        { devicename: 'Printer', hostname: 'printer.local' }
+    ],
+    projectName: 'RicohSpeak',
+    user: 'root',
+    password: '12345678',
+    startFile: 'app.js'
+}
